cbde8cb22c237319a92b4a9a3510c26873948da6
Alvin Rao <alvinrao7@gmail.com>
2019-01-15 02:49:49 +0000

    can add URLs

---
+++ options.js
@@ -0,0 +1,62 @@
+var https = require('https');
+
+function getAndPrintHTML(url) {
+  https.get(url, function(response) {
+    if (response.statusCode != 200) {
+      throw new Error('Request Failed with Status Code' + response.statusCode);
+      return;
+    }
+
+
+  // set encoding of received data to UTF-8 + concatenates with a newline character
+  // the callback is invoked when a `data` chunk is received
+    // var body = '';
+    response.setEncoding('utf8');
+    var entireText = '';
+    response.on('data', function(chunk) {
+      entireText += chunk;
+      console.log('Chunk Received. Length:', chunk.length);
+    });
+
+  // // the callback is invoked when all of the data has been received
+    response.on('end', function() {
+      console.log(entireText)
+      console.log('Response stream complete.');
+    });
+  });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+
+var pageNumbers = [1,2,3].forEach(function(number) {
+  var url = 'https://sytantris.github.io/http-examples/step' + number + '.html';
+  getAndPrintHTML(url)
+});
+
+
+
+// var requestOptions = {
+//   host: 'sytantris.github.io',
+//   path: '/http-examples/step3.html'
+// };
+
+
+
+// function getAndPrintHTML (options) {
+//   var body = '';
+//     response.setEncoding('utf8');
+//     response.on('data', function(chunk) {
+//       body += chunk;
+//       console.log('Chunk Received. Length:', data.length);
+//   });
+//     response.on('end', function (data) {
+//       console.log(data);
+//       callback(null, body);
+// }
+
+
+
